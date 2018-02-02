ce8502b9f3b385ba03943018952b4a04ba45d724
Heitor Chang <heitorchang@gmail.com>
2018-02-02 16:30:29 -0200

    clientes crud

--- registros/urls.py
+++ registros/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('clientes/novo', views.clientesNovo, name='clientesNovo'),
     path('clientes/ver/<int:cliente_id>', views.clientesVer, name='clientesVer'),
     path('clientes/editar/<int:cliente_id>', views.clientesEditar, name='clientesEditar'),
+    path('clientes/confirmar_apagar/<int:cliente_id>', views.clientesConfirmarApagar, name='clientesConfirmarApagar'),
     path('clientes/apagar/<int:cliente_id>', views.clientesApagar, name='clientesApagar'),
 ]
--- registros/views.py
+++ registros/views.py
@@ -12,13 +12,11 @@ def index(request):
 # PRODUTO
 
 def produtos(request):
-    context = {'location': 'produtos'}
-    return render(request, 'registros/produtos.html', context)
+    return render(request, 'registros/produtos.html')
 
 
 def produtosVer(request):
-    context = {'location': 'produtos'}
-    return render(request, 'registros/index.html', context)
+    return render(request, 'registros/index.html')
 
 
 # CLIENTE
@@ -26,14 +24,13 @@ def produtosVer(request):
 def clientes(request):
     clientes = Empresa.objects.all().order_by('nome')
     
-    context = {'location': 'clientes',
-               'clientes': clientes}
+    context = {'clientes': clientes}
     return render(request, 'registros/bootstrap/clientes.html', context)
 
 
 def clientesNovo(request):
-    context = {'location': 'clientes'}
-
+    context = {}
+    
     if request.method == "POST":
         form = ClienteForm(request.POST)
         if form.is_valid():
@@ -69,57 +66,32 @@ def clientesNovo(request):
         context['form'] = form
     return render(request, 'registros/bootstrap/clientesNovo.html', context)
 
-    """
-    # MDC version
-    
-    try:
-        nome = request.POST['nome']
-        rua = request.POST['rua']
-        cidade = request.POST['cidade']
-        estado = request.POST['estado']
-        cep = request.POST['cep']
-        pais = request.POST['pais']
-        telefone = request.POST['telefone']
-        email = request.POST['email']
-        cnpj = request.POST['cnpj']
-        inscricao_est = request.POST['inscricao_est']
-        inscricao_mun = request.POST['inscricao_mun']
-    except KeyError:
-
-        return render(request, 'registros/clientesNovo.html', context)
-    else:
-        cli_novo = Empresa(nome=nome, rua=rua, cidade=cidade,
-                           estado=estado, cep=cep, pais=pais,
-                           telefone=telefone, email=email,
-                           cadastro_nacional=cnpj,
-                           cadastro_estadual=inscricao_est,
-                           cadastro_municipal=inscricao_mun)
-        cli_novo.save()
-        cliente_id = cli_novo.id 
-        return HttpResponseRedirect(reverse('registros:clientesVer', kwargs={'cliente_id': cliente_id})) 
-    """
 
 def clientesVer(request, cliente_id):
     cliente = get_object_or_404(Empresa, pk=cliente_id)
     
-    context = {'location': 'clientes',
-               'cliente': cliente}
+    context = {'cliente': cliente}
     return render(request, 'registros/bootstrap/clientesVer.html', context)
 
 
 def clientesEditar(request, cliente_id):
     cliente = get_object_or_404(Empresa, pk=cliente_id)
 
-    context = {'location': 'clientes',
-               'cliente': cliente}
-    return render(request, 'registros/cliente.html', context)
+    form = ClienteForm(request.POST or None, instance=cliente)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('registros:clientesVer', kwargs={'cliente_id': cliente_id}))
+    return render(request, 'registros/bootstrap/clientesEditar.html', {'form': form})
 
+
+def clientesConfirmarApagar(request, cliente_id):
+    cliente = get_object_or_404(Empresa, pk=cliente_id)
+
+    context = {'cliente': cliente}
+    return render(request, 'registros/bootstrap/clientesConfirmarApagar.html', context)
 
 def clientesApagar(request, cliente_id):
     cliente = get_object_or_404(Empresa, pk=cliente_id)
-    nomeApagado = cliente.nome
+    cliente.delete()
 
-    context = {'location': 'clientes',
-               'nomeApagado': nomeApagado}
-    return render(request, 'registros/clienteApagar.html', context)
-
+    return HttpResponseRedirect(reverse('registros:clientes'))
