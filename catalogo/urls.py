738053a2010983602d086b661473753eea5d45d2
Heitor Chang <heitorchang@gmail.com>
2018-02-09 14:29:27 -0200

    catalogo busca

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -7,4 +7,5 @@ app_name = 'catalogo'
 urlpatterns = [
     path('', views.index, name='index'),
     path('catalogo/lista/<int:lista_id>/', views.lista, name='lista'),
+    path('busca', views.busca, name='busca'),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import MenuPasta, Lista
+from .models import MenuPasta, Lista, Item
 
 def index(request):
     pastas = MenuPasta.objects.all().order_by('nome')
@@ -15,3 +15,15 @@ def lista(request, lista_id):
     
     return render(request, 'catalogo/lista.html',
                   {'pastas': pastas, 'lista_nome': lista.nome, 'itens': itens})
+
+
+def busca(request):
+    query = request.GET['q']
+
+    if query:
+        itens = Item.objects.filter(produto__codigo__icontains=query) | Item.objects.filter(produto__nome__icontains=query)
+    else:
+        itens = Item.objects.none()
+
+    return render(request, 'catalogo/busca_resultado.html',
+                  {'query': query, 'itens': itens})
